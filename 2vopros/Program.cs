17f60dd04c73ce4563e0839b0ef6076755c043c3
grsfr <pavel.ryabykh.05@mail.ru>
2025-12-05 09:08:56 +0300

    Add files via upload

---
+++ 2vopros/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqToObjectsTasks
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("=== Задача 13: Минимальное значение ===");
+            Task13_Min();
+            Console.WriteLine();
+        }
+
+        static void Task13_Min()
+        {
+            int[] numbers = { 3, 7, 2, 9, 1 };
+
+            var min = numbers.Min();
+
+            Console.WriteLine($"Исходный массив: {string.Join(", ", numbers)}");
+            Console.WriteLine($"Минимальное значение: {min}");
+        }
+    }
+}
